3d405132cb1f212f02629064cee45646b6cc194b
Dylan-sepulveda1997 <43625000+Dylan-sepulveda1997@users.noreply.github.com>
2020-09-28 20:47:57 -0300

    valores de salto
    correccion valores de salto

--- Nuevo Proyecto de Juego/player.gd
+++ Nuevo Proyecto de Juego/player.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
 const TARGET_FPS = 60
-const ACCELERATION = 10
-const MAX_SPEED = 1000
-const FRICTION = 10
-const AIR_RESISTANCE = 1
-const GRAVITY = 10
-const JUMP_FORCE = 1000
+
+const ACCELERATION = 15
+const MAX_SPEED = 300
+const FRICTION = 20
+const AIR_RESISTANCE = 0
+const GRAVITY = 25
+const JUMP_FORCE = 600
 
 var motion = Vector2.ZERO
 
@@ -27,11 +28,11 @@ func _physics_process(delta):
 		if x_input == 0:
 			motion.x = lerp(motion.x, 0, FRICTION * delta)
 			
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_select"):
 			motion.y = -JUMP_FORCE
 	else:
 		
-		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+		if Input.is_action_just_released("ui_select") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2
 		
 		if x_input == 0:
