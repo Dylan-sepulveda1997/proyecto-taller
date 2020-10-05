e19e388134667dadbcc34e9e1cb5f7c7a580e883
Dylan-sepulveda1997 <43625000+Dylan-sepulveda1997@users.noreply.github.com>
2020-10-04 23:57:27 -0300

    cosa funciona de nuevo
    ahora si

--- Nuevo Proyecto de Juego/player.gd
+++ Nuevo Proyecto de Juego/player.gd
@@ -1,50 +1,13 @@
 extends KinematicBody2D
 
-<<<<<<< HEAD
-export (int) var run_speed = 300
-export (int) var jump_speed = -400
-export (int) var gravity = 10000000
-
-var velocity = Vector2()
-var jumping = false
-
-func get_input():
-	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_select')
-	
-	if right:
-		$AnimatedSprite.flip_h =true
-		
-	elif left :
-		$AnimatedSprite.flip_h =false
-
-	if jump and is_on_floor():
-		jumping = true
-		velocity.y = jump_speed
-		
-	if right and not left:
-		velocity.x += run_speed
-		
-	if left and not right :
-		velocity.x -= run_speed
-
-func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta*5
-	if jumping and is_on_floor():
-		jumping = false
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-=======
 const TARGET_FPS = 60
 
 const ACCELERATION = 15
 const MAX_SPEED = 200
 const FRICTION = 20
-const AIR_RESISTANCE = 20
-const GRAVITY = 30
-const JUMP_FORCE = 500
+const AIR_RESISTANCE = 0
+const GRAVITY = 25
+const JUMP_FORCE = 600
 
 var motion = Vector2.ZERO
 
@@ -76,7 +39,6 @@ func _physics_process(delta):
 			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
 	
 	motion = move_and_slide(motion, Vector2.UP)
->>>>>>> master
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
